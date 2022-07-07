3228726ca4867d023b900d9964666b8f7ae778ea
徐莹 <xuy@kundeyt.com>
2022-07-07 18:51:28 +0800

    调整代码

--- code/js/index.js
+++ code/js/index.js
@@ -1,55 +1,31 @@
-class lazyMan {
-  constructor(name='') {
-    this.name = name
-    this.task = []
+const FULLFILLED = 'FULLFILLED';
+const REJECTED = 'REJECTED';
+const PENDING = 'PENDING';
 
-    setTimeout(() => {
-      this.next()
-    }, 0)
-  }
+class customPromise {
+  constructor(executor) {
+    this.status = 'PENDING'
+    this.value = undefined
+    this.reason = undefined
 
-  next() {
-    const task = this.tasks.shift() 
-    if (task) task()
-  }
-
-  eat(food) {
-    const task = () => {
-      console.log(`${this.name} eat ${food}`)
-      this.next()
+    let resolve = (value) => {
+      if (this.status === 'PENDING') {
+        
+      }
     }
 
-    this.task.push(task)
-    return this
-  }
+    let reject = reason => {
 
-  sleep(second) {
-    const task = () => {
-      console.log(`${this.name} sleep`)
-      setTimeout(() => {
-        console.log(`${this.name}已经睡完了，开始执行下一个任务`)
-        this.next()
-      }, second * 1000)
     }
-
-    this.tasks.push(task)
-    return this
-  }
-
-
-  sleepFirst(second) {
-    const task = () => {
-      console.log(`${this.name} first sleep`)
-      setTimeout(() => {
-        this.next()
-      }, second * 1000)
+    try {
+      executor(resolve, reject)
+    } catch(e) {
+      reject(e)
     }
-
-    this.tasks.unshift(task)
-    return this
   }
 }
 
 
-const lazyMan = new LazyMan('xuxu')
-
+const promise = new CustomPromise((resolve, reject) => {
+  console.log('111')
+});
--- code/js/promise/01-promise & module/code/index.js
+++
@@ -1,7 +0,0 @@
-var promise1 = new Promise((resolve, reject) => {setTimeout(resolve, 1000)})
-var promise2 = new Promise((resolve, reject) => {setTimeout(resolve, 2000)});
-
-Promise.all([promise1, promise2])
-    .then(function() {
-        console.log('all promise resolved')
-    })
